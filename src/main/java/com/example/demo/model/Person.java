a09dfb5ab2bc9f952202c69fff6eba6b0ba2b444
Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-08-31 12:22:28 +0530

    added postgress data access service

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,24 +1,26 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.NonNull;
 
 import java.util.UUID;
 
 public class Person {
-    private final UUID id;
-    private final String name;
+	private final UUID id;
 
-    public Person(@JsonProperty("id") UUID id,
-                  @JsonProperty("name") String name) {
-        this.id = id;
-        this.name = name;
-    }
+	@NonNull
+	private final String name;
 
-    public UUID getId() {
-        return id;
-    }
+	public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public UUID getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
